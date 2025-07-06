922121d7fcd78f52fae2783e50ef90435ac3dd7e
kevin barany <kevbarany@hotmail.com>
2025-07-06 15:33:57 -0400

    added three.js

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <MainContainer>
+      <HeroSection></HeroSection>
+    </MainContainer>
+  );
 };
+
+const MainContainer = styled.div`
+  background: linear-gradient(135deg, #4e00c2, #8e2de2, #a770ef);
+  height: 100vh;
+  width: 100%;
+  overflow-y: auto;
+  padding-bottom: 100px;
+`;
+
+const HeroSection = styled.div``;
 
 export default Home;
